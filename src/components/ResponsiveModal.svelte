4664292c30d335e9cda2b9d937e72f10a092f51e
miju-Park <gi6238@naver.com>
2024-11-18 21:44:22 +0900

    chore: shadcn-drawer 추가

---
+++ src/components/ResponsiveModal.svelte
@@ -0,0 +1,54 @@
+<script lang="ts">
+	import { mediaQuery } from 'svelte-legos';
+	import * as Dialog from '$lib/components/ui/dialog/index.js';
+	import * as Drawer from '$lib/components/ui/drawer/index.js';
+	import { Input } from '$lib/components/ui/input/index.js';
+	import { Label } from '$lib/components/ui/label/index.js';
+	import { Button } from '$lib/components/ui/button/index.js';
+
+	export let open = false;
+	const isDesktop = mediaQuery('(min-width: 768px)');
+	export let triggerTargetValue = '';
+	export let triggerClassName = '';
+	export let title = '';
+	export let description = '';
+</script>
+
+{#if $isDesktop}
+	<Dialog.Root bind:open>
+		<Dialog.Trigger asChild let:builder>
+			<Button variant="outline" class={triggerClassName} builders={[builder]}
+				>{triggerTargetValue}</Button
+			>
+		</Dialog.Trigger>
+		<Dialog.Content class="sm:max-w-[425px]">
+			<Dialog.Header>
+				<Dialog.Title>{title}</Dialog.Title>
+				<Dialog.Description>
+					{description}
+				</Dialog.Description>
+			</Dialog.Header>
+			<slot />
+		</Dialog.Content>
+	</Dialog.Root>
+{:else}
+	<Drawer.Root bind:open>
+		<Drawer.Trigger asChild let:builder>
+			<Button variant="outline" builders={[builder]}>{triggerTargetValue}</Button>
+		</Drawer.Trigger>
+		<Drawer.Content>
+			<Drawer.Header class="text-left">
+				<Drawer.Title>{title}</Drawer.Title>
+				<Drawer.Description>
+					{description}
+				</Drawer.Description>
+			</Drawer.Header>
+			<slot />
+			<Drawer.Footer class="pt-2">
+				<Drawer.Close asChild let:builder>
+					<Button variant="outline" builders={[builder]}>Cancel</Button>
+				</Drawer.Close>
+			</Drawer.Footer>
+		</Drawer.Content>
+	</Drawer.Root>
+{/if}
